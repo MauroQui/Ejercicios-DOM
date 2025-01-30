218ee2c57341de7d6bd5570ad7719a99c2414735
MauroQui <pmq244@gmail.com>
2025-01-29 22:04:27 -0500

    Boton activar/detener audio funcionando

--- js/index_dom.js
+++ js/index_dom.js
@@ -4,6 +4,8 @@ import { digitalClock, alarm } from "./dom/reloj.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e => {
-  hamburgerMenu(".panel-btn", ".panel", ".menu a") 
-  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
+  hamburgerMenu(".panel-btn", ".panel", ".menu a"); 
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarm("assets/lofi-alarm-clock.mp3","#activar-alarma", "#desactivar-alarma");
+
 });
