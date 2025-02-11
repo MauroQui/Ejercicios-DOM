ba700ccb82799ce8c5e0a135008881478df7850f
MauroQui <pmq244@gmail.com>
2025-02-11 16:30:17 -0500

    Prueba responsive ancho, alto, botones probar y cerrar pagina

--- js/dom/cuenta_regresiva.js
+++ js/dom/cuenta_regresiva.js
@@ -12,12 +12,12 @@ export default function countdown(id, limitDate, finalMessage) {
         minutes = ("0"+ Math.floor(limitTime % (1000 * 60 * 60) / (1000 * 60))).slice(-2),
         seconds = ("0"+ Math.floor(limitTime % (1000 * 60) / (1000))).slice(-2);
 
-    $countdown.innerHTML = `<h3>Faltan: ${days} dias ${hours} horas ${minutes} minutes ${seconds} segundos </h3>`;    
-    //console.log(countdownDate, now, limitTime);
+        $countdown.innerHTML = `<h3>Faltan: ${days} dias ${hours} horas ${minutes} minutes ${seconds} segundos </h3>`;    
+        //console.log(countdownDate, now, limitTime);
 
-    if(limitTime < 0)
-      clearInterval(countdownTempo);
-    $countdown.innerHTML = `<h3> ${finalMessage} </h3>`;
+      if(limitTime < 0)
+        clearInterval(countdownTempo);
+        //$countdown.innerHTML = `<h3> ${finalMessage} </h3>`;
 
   },1000);
 }
---
+++ js/dom/prueba_responsive.js
@@ -0,0 +1,20 @@
+const d = document;
+
+export default function responsiveTester(form) {
+  const $form = d.getElementById(form);
+  let tester;
+
+  d.addEventListener("submit", e => {
+    if(e.target === $form) {
+      e.preventDefault();
+      //alert("Formulario Enviado");
+      tester = window.open($form.direccion.value, "tester", `innerWidth = ${$form.ancho.value}, innerHeigth = ${$form.alto.value } `);
+    }
+  });
+
+  d.addEventListener("click", e => {
+    if(e.target === $form.cerrar) {
+      tester = window.close();
+    }
+  });
+}
