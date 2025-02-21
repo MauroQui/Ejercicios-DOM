c46fe7d66dfacfaa460f22003b728b0be52494aa
MauroQui <pmq244@gmail.com>
2025-02-20 19:13:00 -0500

    Geolocalizacion

--- js/index_dom.js
+++ js/index_dom.js
@@ -3,6 +3,7 @@ import countdown from "./dom/cuenta_regresiva.js";
 import userDeviceInfo from "./dom/deteccion_dispositivos.js";
 import networkStatus from "./dom/deteccion_red.js";
 import webCam from "./dom/deteccion_webcam.js";
+import getGeolocation from "./dom/geolocalizacion.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import responsiveMedia from "./dom/objeto_responsive.js";
 import responsiveTester from "./dom/prueba_responsive.js";
@@ -39,6 +40,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
 
   webCam("webcam");
   
+  getGeolocation("geolocation")
 });
 
 d.addEventListener("keydown", (e) => {
