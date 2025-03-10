556d17c9e5ca768dc5d233ac7766111e3fb279e7
MauroQui <pmq244@gmail.com>
2025-03-09 19:49:53 -0500

    Validaciones del Formulario y simulacion entrega a nuestro correo

--- js/index_dom.js
+++ js/index_dom.js
@@ -14,6 +14,7 @@ import scrollSpy from "./dom/scroll_espia.js";
 import draw from "./dom/sorteo.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
 import darkTheme from "./dom/tema_oscuro.js";
+import conctactFormValidations from "./dom/validaciones_formulario.js";
 import smartVideo from "./dom/video_inteligente.js";
 
 const d = document;
@@ -56,6 +57,10 @@ d.addEventListener("DOMContentLoaded", (e) => {
   scrollSpy();
 
   smartVideo();
+
+  conctactFormValidations();
+
+  
 });
 
 d.addEventListener("keydown", (e) => {
